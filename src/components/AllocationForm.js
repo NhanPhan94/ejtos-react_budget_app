e33365ce93b92194dfee6e43d8a2fd13aa85b653
NhanPhan <ptnhan1200117@gmail.com>
2024-01-22 10:52:55 -0800

    Added currency changing

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch,remaining, currency  } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -15,6 +15,7 @@ const AllocationForm = (props) => {
                 setCost("");
                 return;
             }
+            
 
         const expense = {
             name: name,
@@ -58,7 +59,7 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                  <span>{currency}</span>
                     <input
                         required='required'
                         type='number'
@@ -66,8 +67,7 @@ const AllocationForm = (props) => {
                         value={cost}
                         style={{ marginLeft: '2rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
-                        </input>
-
+                        </input>                  
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
                         Save
                     </button>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,19 +1,44 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect} from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget, dispatch } = useContext(AppContext);
+    
+    const { budget, dispatch, expenses, currency} = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    // const handleBudgetChange = (event) => {
+    //     setNewBudget(event.target.value);
+    //     dispatch({ type: 'SET_BUDGET', payload: newBudget });
+    // }
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-        dispatch({ type: 'SET_BUDGET', payload: newBudget });
+        const updatedBudget = parseFloat(event.target.value);
+        setNewBudget(updatedBudget);
+        dispatch({ type: 'SET_BUDGET', payload: updatedBudget });
     }
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
+    useEffect(() => {
+        if (totalExpenses > budget) {
+            alert("You cannot reduce the budget value lower than spending!!");
+            setNewBudget(budget); // Reset the input value to the current budget
+        }
+        if(budget > 20000){
+            alert('You cannot increase the budget value higher than 20000');
+            setNewBudget(budget); // Reset the input value to the current budget
+        }
+    }, [budget, totalExpenses, currency]);
+
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £ </span>
+<span>Budget: {currency} </span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
 };
 export default Budget;
+
+// const { expenses, budget } = useContext(AppContext);
+//     const totalExpenses = expenses.reduce((total, item) => {
+//         return (total = total + item.cost);
+//     }, 0);
 
